01d64359548f0d96cb4e18739cfb9bbf1cefd283
jsedano77 <123785468+jsedano77@users.noreply.github.com>
2023-04-10 16:30:18 -0600

    arreglos

---
+++ Modulo5-Actividad1.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[12]:
+
+
+n={15,30,45}
+
+
+# In[13]:
+
+
+type(n)
+
+
+# m=np.array(n)
+
+# In[14]:
+
+
+nl=list(n)
+
+
+# In[15]:
+
+
+m=np.array(nl)
+
+
+# In[16]:
+
+
+type(m)
+
+
+# In[20]:
+
+
+print(str(m))
+
+
+# In[18]:
+
+
+a=np.array([0, 1, 2, 3, 4, 0, 1, 2, 3, 4])
+
+
+# In[19]:
+
+
+a
+
+
+# In[21]:
+
+
+a.sort()
+
+
+# In[22]:
+
+
+a
+
+
+# In[ ]:
+
+
+
+
